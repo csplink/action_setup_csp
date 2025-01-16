b2ac14191c0a3b82dc23b0098dc5cc7e25ad056a
xqyjlj <xqyjlj@126.com>
2025-01-16 16:10:54 +0800

    🐳 chore: 删除 axios 库
    Signed-off-by: xqyjlj <xqyjlj@126.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+/**
+ * ****************************************************************************
+ *  @author      xqyjlj
+ *  @file        index.js
+ *  @brief
+ *
+ * ****************************************************************************
+ *  @attention
+ *  Licensed under the GNU General Public License v. 3 (the "License");
+ *  You may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      https://www.gnu.org/licenses/gpl-3.0.html
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ *
+ *  Copyright (C) 2025-2025 xqyjlj<xqyjlj@126.com>
+ *
+ * ****************************************************************************
+ *  Change Logs:
+ *  Date           Author       Notes
+ *  ------------   ----------   -----------------------------------------------
+ *  2025-01-16     xqyjlj       initial version
+ */
 const core = require('@actions/core');
 const github = require('@actions/github');
 const tc = require('@actions/tool-cache');
@@ -5,7 +33,6 @@ const https = require('https');
 const fs = require('fs');
 const path = require('path');
 const os = require('os');
-const axios = require('axios');
 const {exec} = require('@actions/exec');
 
 const version = core.getInput('version') || 'latest'
